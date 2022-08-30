894d38a4d000b18920b037b904c1adc96dbae4b7
Marcelo Mendonça <mendonca2709@gmail.com>
2022-08-30 18:01:21 -0300

    Strip quotes from configmap

--- configmap.tf
+++ configmap.tf
@@ -7,58 +7,60 @@ resource "kubernetes_manifest" "agent" {
       namespace = var.namespace
     }
     data = {
-      "agent.yaml" = yamlencode({
-        server = {
-          log_level  = "info"
-          log_format = "json"
-        }
-        traces = {
-          configs = [{
-            name = "default"
-            attributes = {
-              actions = [{
-                key    = "env"
-                action = "upsert"
-                value  = var.environment
-                }, {
-                key    = "project"
-                action = "upsert"
-                value  = var.project
-              }]
-            }
-            batch = {
-              send_batch_size = 1000
-              timeout         = "5s"
-            }
-            remote_write = concat([{
-              endpoint = var.tempo_endpoint
-              basic_auth = {
-                username = var.tempo_username
-                password_file : "/run/secrets/grafana/${var.tempo_api_key_secret.key}"
+      "agent.yaml" = replace(
+        yamlencode({
+          server = {
+            log_level  = "info"
+            log_format = "json"
+          }
+          traces = {
+            configs = [{
+              name = "default"
+              attributes = {
+                actions = [{
+                  key    = "env"
+                  action = "upsert"
+                  value  = var.environment
+                  }, {
+                  key    = "project"
+                  action = "upsert"
+                  value  = var.project
+                }]
               }
-              retry_on_failure = {
-                enabled : true
+              batch = {
+                send_batch_size = 1000
+                timeout         = "5s"
               }
-            }], var.additional_remote_writes)
-            receivers = {
-              otlp = {
-                protocols = {
-                  grpc = {
-                    include_metadata : true
+              remote_write = concat([{
+                endpoint = var.tempo_endpoint
+                basic_auth = {
+                  username = var.tempo_username
+                  password_file : "/run/secrets/grafana/${var.tempo_api_key_secret.key}"
+                }
+                retry_on_failure = {
+                  enabled : true
+                }
+              }], var.additional_remote_writes)
+              receivers = {
+                otlp = {
+                  protocols = {
+                    grpc = {
+                      include_metadata : true
+                    }
                   }
                 }
               }
-            }
-            automatic_logging = {
-              backend = "stdout"
-              roots : true
-              overrides = {
-                trace_id_key : "trace_id"
+              automatic_logging = {
+                backend = "stdout"
+                roots : true
+                overrides = {
+                  trace_id_key : "trace_id"
+                }
               }
-            }
-          }]
-        }
-      })
+            }]
+          }
+        }),
+      "\"", "")
     }
   }
 }
